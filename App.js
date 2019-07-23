6d722d29454c055426e827ab075d915d643d5edb
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-07-23 11:14:52 -0700

    remove yellowbox warnings

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 import Main from './src/Main'
+import { YellowBox } from 'react-native'
+
+YellowBox.ignoreWarnings([
+  'Possible Unhandled Promise Rejection',
+  'Remote debugger'
+])
 
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
