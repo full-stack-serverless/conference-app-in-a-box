3ba542d458a63d49759ee570f178bfa37df4e04d
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-07-23 11:02:13 -0700

    added yellowbox ignore

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+
+YellowBox.ignoreWarnings([
+  'Possible Unhandled Promise Rejection',
+  'Remote debugger'
+])
 
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
